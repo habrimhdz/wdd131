f130782cbf10078c8746b62c40e38376c5ffa6ee
habrimhdz <habrimhdz@gmail.com>
2025-08-15 13:19:30 -0600

    Siteplan in progress

---
+++ project/scripts/getdates.js
@@ -0,0 +1,8 @@
+document.getElementById("lastModified").textContent = document.lastModified
+
+const today = new Date();
+document.getElementById("currentyear").textContent = today.getFullYear();
+const currentyear = today.getFullYear();
+console.log(year);
+
+
